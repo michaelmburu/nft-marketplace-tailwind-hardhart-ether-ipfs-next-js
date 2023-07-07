12b4513798de0be42c58a474b6e05506d64bb213
Michael Mburu <ichangaimichael@yahoo.com>
2023-07-07 14:10:12 +0300

    - Added Navigation Routers - Added Dark Mode toggle

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import Script from 'next/script'
+import { Navbar, Footer } from '../components'
+Script
 import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute='class'>
+    <div className='dark: bg-nft-dark bg-white min-h-screen'>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script
+      src='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js'
+      integrity='sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow=='
+      crossorigin='anonymous'
+      referrerpolicy='no-referrer'
+    />
+  </ThemeProvider>
+)
 
 export default MyApp
