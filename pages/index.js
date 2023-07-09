423291d21bd8be5f43cc70c3cd0d7c4b3499307d
Michael Mburu <ichangaimichael@yahoo.com>
2023-07-09 11:08:36 +0300

    - Added home page banner

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import { Banner } from '../components'
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Welcome</h1>
+    <div className='flex justify-center sm:px-4v p-12'>
+      <div className='w-full minmd:w-4/5'>
+        <Banner
+          name='Discover, collect, and sell extraordinary NFTs'
+          childStyles='md:text-4xl sm:text-2xl xs:text-xl text-left'
+          parentStyles='justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl'
+        />
+      </div>
     </div>
   )
 }
